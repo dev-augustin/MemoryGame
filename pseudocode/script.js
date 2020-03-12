9d98acbb9678d3f0165f9cf0f8e8c3d62a0bccb8
dev-augustin <dev.augustin00@gmail.com>
2020-03-11 19:17:30 -0500

    added pseudocode

---
+++ pseudocode/script.js
@@ -0,0 +1,31 @@
+/* Game Start
+   All tiles face down 
+   Player 1 starts the game 
+       CLICK tile 1 and tile 2
+         IF tile 1 and tile2 matches
+           Player 1 scores 1 point
+           MATCHED tiles face up
+           Player 1 continues to play 
+         ELSE IF tile 1 && tile 2 do not match
+            OPENED tiles face down
+     Player 2 starts the game
+         CLICK tile 1 and tile 2
+         IF tile 1 and tile2 matches
+           Player 2 scores 1 point
+           MATCHED tiles face up
+           Player 1 continues to play 
+         ELSE IF tile 1 && tile 2 do not match
+            OPENED tiles face down
+      
+      CHECK number of tiles FACE UP
+      IF all tiles face up
+        COMPARE Player 1 and Player 2 score
+            IF Player 1 score EQUALS Player 2 score
+              DECLARE GAME IS TIE
+            ELSE IF Player 1 score > than Player 2 score
+               DECLARE Player 1 WINS    
+            ELSE 
+               DECLARE Player 2 WINS
+      ELSE
+        Game conitues until all cards FACE UP
+       */
