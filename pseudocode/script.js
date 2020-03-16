ea0fc1de6f085a4598ba62b89c345a5127053c29
dev-augustin <dev.augustin00@gmail.com>
2020-03-16 15:03:09 -0500

    updated pseudocode with scoring option" wq exit "

--- pseudocode/script.js
+++ pseudocode/script.js
@@ -4,20 +4,34 @@
        CLICK tile 1 and tile 2
          IF tile 1 and tile2 matches
            MATCHED tiles face up
+             PASSES the turn to Player 2
           
          ELSE IF tile 1 && tile 2 do not match
             OPENED tiles face down
+              PASSES the turn to Player 2
+
      Player 2 starts the game
          CLICK tile 1 and tile 2
          IF tile 1 and tile2 matches
            MATCHED tiles face up
+              PASSES the turn to Player 1
         
          ELSE IF tile 1 && tile 2 do not match
             OPENED tiles face down
+              PASSES the turn to Player 1
     
       CHECK number of tiles FACE UP
       IF all tiles face up
         GAME OVER
       ELSE
         Game conitues until all cards FACE UP
+
+      COMPARE SCORES of Player 1 and Player 2
+      IF Player 1 score > than Player 2
+        Player 1 wins
+      ELSE IF Player 2 score > Player 1
+        Player 2 wins
+      ELSE Game is a Tie   
+
+
        */
